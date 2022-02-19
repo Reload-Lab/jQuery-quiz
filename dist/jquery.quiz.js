c2ce9e9ae3e1b29f8a21d098de20cec14bb13fa9
Domenico Gigante <domenico.gigante@reloadlab.it>
2022-02-19 20:28:05 +0100

    Add template engine function

--- dist/jquery.quiz.js
+++ dist/jquery.quiz.js
@@ -835,6 +835,40 @@
 			document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 		}
 		
+		
+		/*** TEMPLATE ENGINE ***/
+		// JavaScript template engine by Krasimir Tsonev
+		// https://krasimirtsonev.com/blog/article/Javascript-template-engine-in-just-20-line
+		function TemplateEngine(html, options)
+		{
+			var re = /<%([^%>]+)?%>/g, 
+				reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g, 
+				code = 'var r=[];\\n', 
+				cursor = 0, 
+				match;
+			
+			function add(line, js)
+			{
+				js? (code += line.match(reExp)? 
+					line + '\\n' : 'r.push(' + line + ');\\n'):
+					(code += line != '' ? 'r.push("' + line.replace(/"/g, '\\\\"') + '");\\n' : '');
+				
+				return add;
+			}
+			
+			while(match = re.exec(html)){
+				
+				add(html.slice(cursor, match.index))(match[1], true);
+				cursor = match.index + match[0].length;
+			}
+			
+			add(html.substr(cursor, html.length - cursor));
+			
+			code += 'return r.join("");';
+			
+			return new Function(code.replace(/[\\r\\t\\n]/g, '')).apply(options);
+		}
+		
 	
 		/*** INIT ***/
 		init();
